e526d360d19b5c3c595682666e74a8c26bf705c6
Sylvester Lan <sylvesterlan100@gmail.com>
2015-12-13 17:32:43 -0800

    added files

---
+++ example2/script.js
@@ -0,0 +1,13 @@
+// The following javascript must be used by an html file
+// Javascript is useless by itself
+
+// Arrays
+
+    [1, 2, 3, 4, 5, 12, 53, 3, 12, 3];
+    ["hi", "this", "is", "an", "array"];
+    [4, "mixed", 31, "32", "up", true];
+
+// You can console.log arrays
+
+	var arr = [2, 4, "displayed"]
+	console.log(arr);
